35a1de0c23ca03982c4bc21486e41dc2464f0786
Vítor Eulalio Reis <reis.vitore@gmail.com>
2020-12-05 19:30:59 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -166,7 +166,77 @@ class QuizzBee extends Component {
     }
 }
 
+class QuizzBee1 extends Component {
+
+    state = {
+        questionBank: [],
+        score: 0,
+        responses: 0,
+        number: 5
+    };
+
+    getQuestions = () => {
+        axios.get(process.env.REACT_APP_SERVER_URL + '/' + this.state.number)
+            .then(result => {
+                this.setState({questionBank : result.data})
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    computeAnswer = ( id, answer ) => {
+        axios.post( process.env.REACT_APP_SERVER_URL + '/' +id + '/'+ answer )
+        .then(result => {
+            if(result.data){
+                this.setState({ score: this.state.score + 1 });
+            }
+            this.setState({responses: this.state.responses < this.state.number ? this.state.responses + 1 : this.state.number});
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+
+    componentDidMount() {
+        this.getQuestions();
+
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState(
+            {
+                score: 0,
+                responses: 0
+            });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="title">CIC-Quiz</div>
+                {this.state.questionBank.length > 0 &&
+                    this.state.responses < this.state.number &&
+                    this.state.questionBank.map(
+                        ({ question, answers, correct, id }) => (
+                            <QuestionBox
+                                question={question}
+                                options={answers}
+                                key={id}
+                                selected={(answer) => this.computeAnswer(id, answer)} />
+                        )
+                    )
+                }
+                {this.state.responses === this.state.number ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
+            </div>
+        );
+    }
+}
+
+
 ReactDOM.render(
   <QuizzBee />,
+  <QuizzBee1 />,
   // <PlayGround/>,
   document.getElementById('root'));
