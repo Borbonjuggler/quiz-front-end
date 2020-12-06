4b028be1c0872bfe9ef383cd535a03ec337c9f1f
Vítor Eulalio Reis <reis.vitore@gmail.com>
2020-12-06 15:48:43 +0100

    cant assemble data

---
+++ src/components/MemGame1.js
@@ -0,0 +1,133 @@
+import React, {Component} from "react";
+import Card from '../containers/Card';
+import axios from 'axios';
+
+
+class MemGame1 extends Component {
+
+    state = {
+        cardsBank: [],
+        score: 0,
+        responses: 0,
+        frameworks: ['angular2','vue','react','grunt','phantomjs','ember','babel','ionic','gulp','meteor','yeoman','yarn','nodejs','bower','browserify'],
+        duplicatedFrameworks: [],
+        randomizedFrameworks: [],
+        finalizedFrameworks: [],
+        openedFrameworks: [],
+        number: 5
+    };
+
+    getCards = () => {
+        axios.get(process.env.REACT_APP_SERVER_URL + '/cards')
+            .then(result => {
+                console.log("RES",result)
+                this.setState({cardsBank : result.data})
+                this.start()
+            })
+            .catch(error => {
+              console.log("ERR",error);
+                console.log(error);
+            });
+    };
+
+
+    handleClick(name,index){
+      console.log("STATE BEFORE: ", this.state);
+      if(this.state.openedFrameworks.length === 2){
+        setTimeout(() => {
+          this.check()
+        },750)
+      }else {
+        let framework = {
+          name,
+          index
+        }
+        let finalizedFrameworks = this.state.finalizedFrameworks
+        let frameworks = this.state.openedFrameworks
+        finalizedFrameworks[index].close = false
+        frameworks.push(framework)
+        this.setState({
+          openedFrameworks: frameworks,
+          finalizedFrameworks: finalizedFrameworks
+        })
+        if(this.state.openedFrameworks.length === 2){
+          setTimeout(() => {
+            this.check()
+          },750)
+        }
+      }
+      console.log("STATE AFTER: ", this.state);
+    }
+
+    check(){
+      let finalizedFrameworks = this.state.finalizedFrameworks
+      if((this.state.openedFrameworks[0].name === this.state.openedFrameworks[1].name) && (this.state.openedFrameworks[0].index !== this.state.openedFrameworks[1].index)){
+        finalizedFrameworks[this.state.openedFrameworks[0].index].complete = true
+        finalizedFrameworks[this.state.openedFrameworks[1].index].complete = true
+      }else {
+        finalizedFrameworks[this.state.openedFrameworks[0].index].close = true
+        finalizedFrameworks[this.state.openedFrameworks[1].index].close = true
+      }
+      this.setState({
+        finalizedFrameworks,
+        openedFrameworks: []
+      })
+    }
+
+    start(){
+      let finalizedFrameworks = [];
+      this.state.duplicatedFrameworks = this.state.frameworks.concat(this.state.frameworks)
+      this.state.randomizedFrameworks = this.shuffle(this.state.duplicatedFrameworks)
+      this.state.randomizedFrameworks.map((name,index) => {
+        finalizedFrameworks.push({
+          name,
+          close: true,
+          complete: false,
+          fail: false
+        })
+      })
+      this.state.finalizedFrameworks = finalizedFrameworks
+      // console.log("STATE: ", this.state);
+    }
+
+    shuffle(array){
+      let currentIndex = array.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array
+    }
+
+    componentDidMount() {
+        this.getCards();
+
+    }
+
+    playAgain = () => {
+        this.getCards();
+        this.setState(
+            {
+                score: 0,
+                responses: 0
+            });
+    }
+
+    render() {
+      console.log("cards: ", this.state.cardsBank);
+        return (
+          <div className="memgame">
+              {
+                this.state.cardsBank.map((framework, index) => {
+                  return <Card key={framework.index} framework={framework.name} click={() => {this.handleClick(framework.name,index)}} close={framework.close} complete={framework.complete}/>
+                })
+              }
+          </div>
+        );
+    }
+}
+
+export default MemGame1;
