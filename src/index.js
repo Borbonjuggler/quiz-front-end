cd7ebf591823bdadaa4bd02f5a9098b834e29937
Vítor Eulalio Reis <reis.vitore@gmail.com>
2020-12-06 09:46:36 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -2,100 +2,100 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; //webpage
 import './assets/style.css';
 import QuestionBox from './components/QuestionBox';
-import Card from './components/MemGame';
+// import Card from './components/MemGame';
 
 import Result from './components/Result'
 import axios from 'axios';
 
 
-class PlayGround extends Component {
-
-      state = {
-        frameworks: ['angular2','vue','react','grunt','phantomjs','ember','babel','ionic','gulp','meteor','yeoman','yarn','nodejs','bower','browserify'],
-        duplicatedFrameworks: [],
-        randomizedFrameworks: [],
-        finalizedFrameworks: [],
-        openedFrameworks: []
-      };
-      // this.start()
-    // }
-    // handleClick(name,index){
-    //   if(this.state.openedFrameworks.length === 2){
-    //     setTimeout(() => {
-    //       this.check()
-    //     },750)
-    //   }else {
-    //     let framework = {
-    //       name,
-    //       index
-    //     }
-    //     let finalizedFrameworks = this.state.finalizedFrameworks
-    //     let frameworks = this.state.openedFrameworks
-    //     finalizedFrameworks[index].close = false
-    //     frameworks.push(framework)
-    //     this.setState({
-    //       openedFrameworks: frameworks,
-    //       finalizedFrameworks: finalizedFrameworks
-    //     })
-    //     if(this.state.openedFrameworks.length === 2){
-    //       setTimeout(() => {
-    //         this.check()
-    //       },750)
-    //     }
-    //   }
-    // }
-    check(){
-      let finalizedFrameworks = this.state.finalizedFrameworks
-      if((this.state.openedFrameworks[0].name === this.state.openedFrameworks[1].name) && (this.state.openedFrameworks[0].index !== this.state.openedFrameworks[1].index)){
-        finalizedFrameworks[this.state.openedFrameworks[0].index].complete = true
-        finalizedFrameworks[this.state.openedFrameworks[1].index].complete = true
-      }else {
-        finalizedFrameworks[this.state.openedFrameworks[0].index].close = true
-        finalizedFrameworks[this.state.openedFrameworks[1].index].close = true
-      }
-      this.setState({
-        finalizedFrameworks,
-        openedFrameworks: []
-      })
-    }
-    // start(){
-    //   let finalizedFrameworks = [];
-    //   this.state.duplicatedFrameworks = this.state.frameworks.concat(this.state.frameworks)
-    //   this.state.randomizedFrameworks = this.shuffle(this.state.duplicatedFrameworks)
-    //   this.state.randomizedFrameworks.map((name,index) => {
-    //     finalizedFrameworks.push({
-    //       name,
-    //       close: true,
-    //       complete: false,
-    //       fail: false
-    //     })
-    //   })
-    //   this.state.finalizedFrameworks = finalizedFrameworks
-    // }
-    // shuffle(array){
-    //   let currentIndex = array.length, temporaryValue, randomIndex;
-    //   while (0 !== currentIndex) {
-    //     randomIndex = Math.floor(Math.random() * currentIndex);
-    //     currentIndex -= 1;
-    //     temporaryValue = array[currentIndex];
-    //     array[currentIndex] = array[randomIndex];
-    //     array[randomIndex] = temporaryValue;
-    //   }
-    //   return array
-    // }
-//     render(){
+// class PlayGround extends Component {
 //
-//       return (
-//         <div className="playground">
-//             {
-//               this.state.finalizedFrameworks.map((framework, index) => {
-//                 return <Card framework={framework.name} click={() => {this.handleClick(framework.name,index)}} close={framework.close} complete={framework.complete}/>
-//               })
-//             }
-//         </div>
-//       )
+//       state = {
+//         frameworks: ['angular2','vue','react','grunt','phantomjs','ember','babel','ionic','gulp','meteor','yeoman','yarn','nodejs','bower','browserify'],
+//         duplicatedFrameworks: [],
+//         randomizedFrameworks: [],
+//         finalizedFrameworks: [],
+//         openedFrameworks: []
+//       };
+//       // this.start()
+//     // }
+//     // handleClick(name,index){
+//     //   if(this.state.openedFrameworks.length === 2){
+//     //     setTimeout(() => {
+//     //       this.check()
+//     //     },750)
+//     //   }else {
+//     //     let framework = {
+//     //       name,
+//     //       index
+//     //     }
+//     //     let finalizedFrameworks = this.state.finalizedFrameworks
+//     //     let frameworks = this.state.openedFrameworks
+//     //     finalizedFrameworks[index].close = false
+//     //     frameworks.push(framework)
+//     //     this.setState({
+//     //       openedFrameworks: frameworks,
+//     //       finalizedFrameworks: finalizedFrameworks
+//     //     })
+//     //     if(this.state.openedFrameworks.length === 2){
+//     //       setTimeout(() => {
+//     //         this.check()
+//     //       },750)
+//     //     }
+//     //   }
+//     // }
+//     check(){
+//       let finalizedFrameworks = this.state.finalizedFrameworks
+//       if((this.state.openedFrameworks[0].name === this.state.openedFrameworks[1].name) && (this.state.openedFrameworks[0].index !== this.state.openedFrameworks[1].index)){
+//         finalizedFrameworks[this.state.openedFrameworks[0].index].complete = true
+//         finalizedFrameworks[this.state.openedFrameworks[1].index].complete = true
+//       }else {
+//         finalizedFrameworks[this.state.openedFrameworks[0].index].close = true
+//         finalizedFrameworks[this.state.openedFrameworks[1].index].close = true
+//       }
+//       this.setState({
+//         finalizedFrameworks,
+//         openedFrameworks: []
+//       })
 //     }
-}
+//     // start(){
+//     //   let finalizedFrameworks = [];
+//     //   this.state.duplicatedFrameworks = this.state.frameworks.concat(this.state.frameworks)
+//     //   this.state.randomizedFrameworks = this.shuffle(this.state.duplicatedFrameworks)
+//     //   this.state.randomizedFrameworks.map((name,index) => {
+//     //     finalizedFrameworks.push({
+//     //       name,
+//     //       close: true,
+//     //       complete: false,
+//     //       fail: false
+//     //     })
+//     //   })
+//     //   this.state.finalizedFrameworks = finalizedFrameworks
+//     // }
+//     // shuffle(array){
+//     //   let currentIndex = array.length, temporaryValue, randomIndex;
+//     //   while (0 !== currentIndex) {
+//     //     randomIndex = Math.floor(Math.random() * currentIndex);
+//     //     currentIndex -= 1;
+//     //     temporaryValue = array[currentIndex];
+//     //     array[currentIndex] = array[randomIndex];
+//     //     array[randomIndex] = temporaryValue;
+//     //   }
+//     //   return array
+//     // }
+// //     render(){
+// //
+// //       return (
+// //         <div className="playground">
+// //             {
+// //               this.state.finalizedFrameworks.map((framework, index) => {
+// //                 return <Card framework={framework.name} click={() => {this.handleClick(framework.name,index)}} close={framework.close} complete={framework.complete}/>
+// //               })
+// //             }
+// //         </div>
+// //       )
+// //     }
+// }
 
 
 class QuizzBee extends Component {
@@ -215,7 +215,7 @@ class QuizzBee1 extends Component {
     render() {
         return (
             <div className="container">
-                <div className="title">CIC-Quiz</div>
+                <div className="title">CIC-Quiz1</div>
                 {this.state.questionBank.length > 0 &&
                     this.state.responses < this.state.number &&
                     this.state.questionBank.map(
@@ -228,7 +228,6 @@ class QuizzBee1 extends Component {
                         )
                     )
                 }
-                {this.state.responses === this.state.number ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
             </div>
         );
     }
@@ -238,5 +237,4 @@ class QuizzBee1 extends Component {
 ReactDOM.render(
   <QuizzBee />,
   <QuizzBee1 />,
-  // <PlayGround/>,
   document.getElementById('root'));
